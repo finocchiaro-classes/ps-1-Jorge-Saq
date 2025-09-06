a2da654aaad252bd682d0a0cd7f23fb7b17a9dfc
Jorge-Saq <jorgesaq01@gmail.com>
2025-09-06 19:34:08 -0400

    ps1 part1-part4

--- ps1-part3.py
+++ ps1-part3.py
@@ -1,1 +1,18 @@
+# function that takes int age and goal
+# Subtracts 220-age for max_HR
+# Print max_HR and target heart rate based on goal
+def heart_rate(age,goal):
+    max_HR = 220-age
+    print(f"Your maximum heart rate is: {max_HR}")
 
+    #range between 80% and 100%
+    if(goal == 'S'):
+        print(f"Your target heart rate is between {(80/100)*max_HR} and {max_HR}")
+    #range between 60% and 80%
+    elif(goal == 'E'):
+        print(f"Your target heart rate is between {(60/100)*max_HR} and {(80/100)*max_HR}")
+
+#Ask users for age and goal and call function
+user_age = int(input("What is your age? "))
+user_goal = input("Is your goal speed (S) or endurance (E)? ")
+heart_rate(user_age, user_goal)
--- ps1-part4.py
+++ ps1-part4.py
@@ -1,1 +1,30 @@
+#Creating variable for recidivism risk
+risk = 0
 
+#Ask for Prior arrest
+prior_arrest = int(input("Prior arrests: "))
+
+#Ask if arrest were local ordinance
+local_ordinance = input("Prior arrests for local ordinance (Y/N): ")
+
+#Ask for age of release
+age_of_release = int(input("Age at release: "))
+
+#Adds points based on prior arrest
+if(prior_arrest >=5):
+    risk+=2
+elif(prior_arrest >=2):
+    risk+=1
+
+#Adds points if prior arrest was for local ordinance
+if(local_ordinance == "Y"):
+    risk+=1
+    
+#Adds/subtracts points based on age of release
+if(age_of_release>=18 and age_of_release<=24):
+    risk+=1
+elif(age_of_release>=40):
+    risk-=1
+    
+#print recidivism risk score
+print(f"The recidivism risk score is {risk}")
